f6b0c9c28833b90ddfebe6c0888a63e3537c363e
MUHAMMED RUFAID E <muhammedrufaide@gmail.com>
2025-08-05 13:34:04 +0530

    Ready for Render deployment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -101,13 +101,4 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import os
-
-DEBUG = os.getenv("DEBUG", "False") == "True"
-SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-default")
-
-ALLOWED_HOSTS = ['*']
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
